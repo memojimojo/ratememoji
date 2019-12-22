c0f637a5174812c4929029a22ccbcf7632e52381
David de Boer <david@ddeboer.nl>
2019-12-22 23:10:53 +0100

    Configure stack

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -1,9 +1,56 @@
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
+import path = require('path');
+import apigateway = require('@aws-cdk/aws-apigateway');
+import ses = require('@aws-cdk/aws-ses');
+import sesactions = require('@aws-cdk/aws-ses-actions');
+import s3 = require('@aws-cdk/aws-s3');
 
 export class ApiStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        const bucket = new s3.Bucket(this, 'EmailBucket');
+
+        const requestUpload = new lambda.Function(this, 'RequestUpload', {
+            code: lambda.Code.fromAsset(path.join(__dirname, '../resources/request-upload')),
+            handler: 'request-upload.handler',
+            runtime: lambda.Runtime.NODEJS_12_X
+        });
+
+        const processEmail = new lambda.Function(this, 'ProcessEmail', {
+            code: lambda.Code.fromAsset(path.join(__dirname, '../resources/process-email')),
+            handler: 'process-email.handler',
+            runtime: lambda.Runtime.NODEJS_12_X,
+            timeout: cdk.Duration.seconds(10),
+            environment: {
+                BUCKET_NAME: bucket.bucketName
+            }
+        });
+
+        bucket.grantRead(processEmail);
+
+        const api = new apigateway.RestApi(this, 'Gateway', {
+            restApiName: 'RateMemoji API'
+        });
+
+        const uploadRequests = api.root.addResource('upload-requests');
+        uploadRequests.addMethod('POST', new apigateway.LambdaIntegration(requestUpload));
+
+        new ses.ReceiptRuleSet(this, 'RuleSet', {
+            rules: [
+                {
+                    recipients: ['doreply@ratememoji.com'],
+                    actions: [
+                        new sesactions.S3({
+                            bucket,
+                        }),
+                        new sesactions.Lambda({
+                            function: processEmail,
+                        }),
+                    ]
+                }
+            ]
+        });
     }
 }
