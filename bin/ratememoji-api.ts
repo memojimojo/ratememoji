8767563a46e562156eb18c6410e984f9f09d21fa
David de Boer <david@ddeboer.nl>
2020-01-10 16:47:02 +0100

    Fix formatting

--- bin/ratememoji-api.ts
+++ bin/ratememoji-api.ts
@@ -4,4 +4,9 @@ import {RateMemoji} from '../lib/ratememoji';
 import cdk = require('@aws-cdk/core');
 
 const app = new cdk.App();
-new RateMemoji(app, 'RateMemoji', {env: {account: process.env.ACCOUNT_ID, region: process.env.REGION}});
+new RateMemoji(app, 'RateMemoji', {
+    env: {
+        account: process.env.ACCOUNT_ID,
+        region: process.env.REGION
+    }
+});
